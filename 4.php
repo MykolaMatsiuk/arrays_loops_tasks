4917d79971c9b65a9d56c2b217bd8d5bafaa320f
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-10-05 12:32:26 +0300

    fourth task

---
+++ 4.php
@@ -0,0 +1,9 @@
+<?php
+$arr = array('green' => 'зеленый', 'red' => 'красный', 'blue' => 'голубой');
+foreach ($arr as $key => $value) {
+  echo "{$key}<br>";
+};
+echo "<br>";
+foreach ($arr as $key => $value) {
+  echo "{$value}<br>";
+}
